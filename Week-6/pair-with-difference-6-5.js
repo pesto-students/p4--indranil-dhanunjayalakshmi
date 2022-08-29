3056615e6e5c8da1d738e6aa1ba1fbc27b4ae804
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2022-08-30 02:15:19 +0530

    Implementing the program to find the pair of elements with the given difference

---
+++ Week-6/pair-with-difference-6-5.js
@@ -0,0 +1,15 @@
+function difference(arr, b) {
+    const set1 =  new Set();
+    for (i in arr) {
+      set1.add(arr[i]);
+    }
+    for (i in arr) {
+      if ((set1.has(b+arr[i])) || (set1.has(arr[i]-b))) {
+          return 1;
+        }
+    }
+    return 0;
+  }
+  
+  console.log(difference([5, 10, 3, 2, 50, 80], 78));
+  
