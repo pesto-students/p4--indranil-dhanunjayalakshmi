d052d187cc17ab65acc9fb2b0861586130b5df67
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2022-09-07 17:54:44 +0530

    Implementing the function to detect the loop in linked list

--- Week-7/detecting-loop-in-ll-7-3.js
+++ Week-7/detecting-loop-in-ll-7-3.js
@@ -1,10 +1,10 @@
 class SLLNode
 {
-  constructor(value)
-  {
-    this.value = value;
-    this.next =  null;
-  }
+    constructor(value)
+    {
+        this.value = value;
+        this.next =  null;
+    }
 }
 
 /**
@@ -14,14 +14,14 @@ class SLLNode
  */
 function createSLL(arr)
 {
-  let head = new SLLNode(arr[0]);
-  let curr =  head;
-  for(let i = 1; i < arr.length; ++i)
-  {
-    curr.next = new SLLNode(arr[i]);
-    curr = curr.next;
-  }
-  return head;
+    let head = new SLLNode(arr[0]);
+    let curr =  head;
+    for(let i = 1; i < arr.length; ++i)
+    {
+        curr.next = new SLLNode(arr[i]);
+        curr = curr.next;
+    }
+    return head;
 }
 
 /**
@@ -30,12 +30,12 @@ function createSLL(arr)
  */
 function traverseSLL(head)
 {
-  let curr = head;
-  while(curr != null)
-  {
-    console.log(curr.value);
-    curr = curr.next;
-  }
+    let curr = head;
+    while(curr != null)
+    {
+        console.log(curr.value);
+        curr = curr.next;
+    }
 }
 
 /**
@@ -44,24 +44,52 @@ function traverseSLL(head)
  * @param {*} k 
  * @returns 
  */
-function createLoop(head, k)
+function createLoop(head, n, k)
 {
-  let pivot = head;
+    let pivot = head;
+    if(k === 0 || k > n) return;
 
-  while(--k)
-  {
-    pivot = pivot.next;
-  }
+    while(--k)
+    {
+        pivot = pivot.next;
+    }
 
-  let tail = pivot;
-  while(tail.next !== null)
-  {
-    tail = tail.next;
-  }
-  tail.next = pivot;
-  return head;
+    let tail = pivot;
+    while(tail.next !== null)
+    {
+        tail = tail.next;
+    }
+    tail.next = pivot;
+    return head;
+}
+
+/**
+ * 
+ * @param {*} head 
+ * @returns 
+ */
+function detectingLoop(head)
+{
+    let slow =  head, fast = head;
+    while(fast && fast.next)
+    {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(slow === fast)
+        {
+            slow =  head;
+            while(slow !== fast)
+            {
+                slow = slow.next;
+                fast =  fast.next;
+            }
+            return true;
+        }
+    }
+    return false;
 }
 
 let head = createSLL([1,2,3,4,5,6,7,8,9,10]);
-head = createLoop(head, 4);
-traverseSLL(head);
+head = createLoop(head, 10, 4);
+console.log(detectingLoop(head));
