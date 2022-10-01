b2b443481958f193ceff0b754d76a536a479b595
dhanunjayalakshmi <dhanunjaya.lakshmi60@gmail.com>
2022-10-01 21:14:23 +0530

    Created basic layout

--- Week-9/todo-app/src/App.js
+++ Week-9/todo-app/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">
+        <h1>Todo App</h1>
+      </div>
+      <div className="inputBox">
+        <form className="inputForm" action="">
+          <input type="text" name="todoTask" placeholder="Add a task" />
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
+      <div className="todotasks">
+        <h2>Todo tasks</h2>
+        <ul>
+          <li>
+            <input type="checkbox" className="checkItem" />
+            <span>List item 1</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="completedTasks">
+        <h2>Completed Tasks</h2>
+        <ul>
+          <li>
+            <input type="checkbox" className="checkItem" />
+            <span>List item 1</span>
+          </li>
+        </ul>
+      </div>
+
+      <footer className="footer"></footer>
     </div>
   );
 }
