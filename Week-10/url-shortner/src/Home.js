986199bf94d07905e9a904f5b46970e864fb0e94
dhanunjayalakshmi <dhanunjaya.lakshmi60@gmail.com>
2022-10-08 11:24:12 +0530

    Implemented the Url shortening logic and added footer to the page

--- Week-10/url-shortner/src/Home.js
+++ Week-10/url-shortner/src/Home.js
@@ -1,5 +1,9 @@
 import "./Home.css";
-function Home() {
+import { useRef } from "react";
+
+function Home({ urls, shortUrl, error }) {
+  const inputValue = useRef(null);
+
   return (
     <div className="Home-section">
       <div className="Home-heading">
@@ -10,23 +14,48 @@ function Home() {
         </p>
       </div>
       <div className="Home-input-box">
-        <form className="Home-input-form" action="">
+        <form
+          className="Home-input-form"
+          action=""
+          onSubmit={(e) => shortUrl(e, inputValue)}
+        >
           <input
             type="text"
             name="shortUrl"
             placeholder="Shorten a link here"
+            ref={inputValue}
           />
           <button type="submit">Shorten it</button>
         </form>
+        <div className="Error-mesg">{error && <p>{error}</p>}</div>
       </div>
       <div className="Home-short-url">
-        <table className="Short-url-list">
-          <tr>
-            <th>Original URL</th>
-            <th>Shortened URL</th>
-            <th></th>
-          </tr>
-        </table>
+        {urls?.length ? (
+          <table className="Short-url-list">
+            <tr>
+              <th>Original URL</th>
+              <th>Shortened URL</th>
+              <th></th>
+            </tr>
+            {urls?.map((el) => (
+              <tr>
+                <td>{el.longUrl}</td>
+                <td>{el.shortenUrl}</td>
+                <td className="Copy-button">
+                  <button
+                    onClick={() => {
+                      navigator.clipboard.writeText(el.shortenUrl);
+                    }}
+                  >
+                    Copy
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </table>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
