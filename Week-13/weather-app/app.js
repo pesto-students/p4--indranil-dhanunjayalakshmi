87ac001dd8d6248a56b0834c4249346ffb4a5c2a
dhanunjayalakshmi <dhanunjaya.lakshmi60@gmail.com>
2022-11-25 19:22:20 +0530

    Added end point to get the current weather of a city by providing its name

--- Week-13/weather-app/app.js
+++ Week-13/weather-app/app.js
@@ -11,4 +11,18 @@ const apiKey = `${process.env.API_KEY}`;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/weather/city", function handler(req, res) {
+  let city = req.query.city;
+
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  request(url, function (err, response, body) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(JSON.parse(body));
+    }
+  });
+});
+
 app.listen(8080);
